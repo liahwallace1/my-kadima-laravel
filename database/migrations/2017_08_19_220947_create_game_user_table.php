f6f2621063ca8fcd65f445b7c6441232adb83cfa
liahwallace1 <liahwallace1@gmail.com>
2017-08-19 18:12:21 -0400

    created game user join migration

---
+++ database/migrations/2017_08_19_220947_create_game_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_user');
+    }
+}
