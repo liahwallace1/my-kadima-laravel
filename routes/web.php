01b1d3226b7085261ae7b1961f2b4d2af8e23665
liahwallace1 <liahwallace1@gmail.com>
2017-08-19 09:58:30 -0400

    started adding routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// root 'static#home'
+
+Route::get('/login', `SessionsController@new`)
+Route::post('/login', `SessionsController@create`)
+Route::get('/logout', `SessionsController@destroy`)
+
+  #omniauth
+  Route::get('/auth/facebook/callback', `SessionsController@create`)
+  Route::get('/auth/failure', function () {
+    return redirect()->route('/')
+  })
+
+  // Other Rails routes
+  // resources :users do
+  //   resources :games
+  // end
+  //
+  // resources :games
+  // resources :locations
