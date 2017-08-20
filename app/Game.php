142b270554a988318178e1e55afc4923987046db
liahwallace1 <liahwallace1@gmail.com>
2017-08-19 21:31:43 -0400

    added relationships to game model

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+  protected $table = 'games';
+  protected $fillable = [
+    'date_played', 'distance', 'game_type', 'volley_total', 'location_id'
+  ];
+
+  public function game_user()
+  {
+    return $this->hasMany('App\GameUser');
+  }
+
+  public function users()
+  {
+    return $this->hasMany('App\User');
+  }
+
+  public function location()
+  {
+    return $this->belongsTo('App\Location');
+  }
 }
