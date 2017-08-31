166ceaa71fc484c7349ee726bf32d162e98c1c90
liahwallace1 <liahwallace1@gmail.com>
2017-08-31 16:57:37 -0400

    Edited game index page

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Locations Page')
 @section('content')
 <div class="container">
+  <h3>Kadima Locations</h3>
   <a class="btn btn-warning btn-group-sm" href="{{ url('/locations/new') }}">Add a Location</a>
   <br /><br />
   <div class="row">
@@ -12,6 +13,9 @@
             <strong><a href="{{ url('/locations/'.$location->id) }}">{{ $location->name }}</a></strong><br />
             {{ $location->city }}, {{ $location->state }}<br />
             <a class="btn btn-default btn-group-xs" href="{{ url('/locations/'.$location->id.'/edit') }}">Edit</a>
+            @if(!isset($location->city) || !isset($location->state) || !isset($location->turf))
+              <p><em>This location needs to be updated.</em></p>
+            @endif
           </li>
         @endforeach
       @else
